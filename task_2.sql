6f58fbd41b6eaa79494775dbe5e43d1a891cd85c
Oby Tonia <anthonian22@gmail.com>
2024-07-28 23:45:57 +0100

    mod.

--- task_2.sql
+++ task_2.sql
@@ -40,13 +40,12 @@ CREATE TABLE Orders (
   FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE order_details (
+CREATE TABLE Order_Details (
   order_id INT,
   book_id INT,
-  quantity INT,   
+  quantity DOUBLE INT,   
 
   PRIMARY KEY (order_id, book_id),
-  FOREIGN KEY (order_id) REFERENCES orders(order_id),
-  FOREIGN KEY (book_id) REFERENCES books(book_id)   
-
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id)
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
