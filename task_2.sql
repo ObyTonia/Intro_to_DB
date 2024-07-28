2cda66a54b5ae5054763d2ce9d6877eefa042f05
Oby Tonia <anthonian22@gmail.com>
2024-07-28 23:35:43 +0100

    modified

--- task_2.sql
+++ task_2.sql
@@ -1,11 +1,9 @@
- -- USE alx_book_store;
-
-CREATE TABLE authors (
+CREATE TABLE Authors (
   author_id INT PRIMARY KEY AUTO_INCREMENT,
   author_name VARCHAR(255)
 );
 
-CREATE TABLE books (
+CREATE TABLE Books (
   book_id INT PRIMARY KEY AUTO_INCREMENT,
   title VARCHAR(255),
   author_id INT,
@@ -13,4 +11,41 @@ CREATE TABLE books (
   publish_date DATE,
   price DECIMAL(10,2),
   FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+CREATE TABLE Customers (
+  customer_id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(50),
+  last_name VARCHAR(50),
+  email VARCHAR(100),
+  address VARCHAR(255),
+  city VARCHAR(50),
+  state VARCHAR(50),   
+
+  zip_code VARCHAR(20),
+  country VARCHAR(50),
+  phone_number VARCHAR(20)
+);
+
+CREATE TABLE Orders (
+  order_id INT PRIMARY KEY AUTO_INCREMENT,
+  customer_id INT,
+  order_date DATE,
+  shipping_address VARCHAR(255),
+  shipping_city VARCHAR(50),
+  shipping_state VARCHAR(50),
+  shipping_zip_code VARCHAR(20),
+  shipping_country VARCHAR(50),
+  FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE Order_details (
+  order_id INT,
+  book_id INT,
+  quantity INT,   
+
+  PRIMARY KEY (order_id, book_id),
+  FOREIGN KEY (order_id) REFERENCES orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES books(book_id)   
+
 );
